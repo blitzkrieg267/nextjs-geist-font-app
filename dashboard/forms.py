121f37693d811e5dc426dd2f01d62176cb698b3c
blitzkrieg267 <117721918+blitzkrieg267@users.noreply.github.com>
2025-07-16 15:27:28 +0200

    Add dashboard/forms.py 1752672446311

---
+++ dashboard/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'is_active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'is_staff': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = None
+        for field_name, field in self.fields.items():
+            if field_name not in ['is_active', 'is_staff']:
+                field.required = True
